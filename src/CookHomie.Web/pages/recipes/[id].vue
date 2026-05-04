58675cac6c9104fbac4e171a63a690921f074992
Rafael Bolaños <animasdelmundo2@gmail.com>
2026-05-03 20:12:06 -0500

    chore: wrong input stucture

--- src/CookHomie.Web/pages/recipes/[id].vue
+++ src/CookHomie.Web/pages/recipes/[id].vue
@@ -64,7 +64,7 @@
           </div>
           <div v-else class="flex flex-col gap-3">
             <SharedFormField label="Name">
-              <input v-model="editBuffer.name" class="input"></input>
+              <input v-model="editBuffer.name" class="input">
             </SharedFormField>
             <SharedFormField label="Tags (comma separated)">
             <input
